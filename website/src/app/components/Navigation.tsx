1a7f10d1ba3fd9b5d48e84679d400a505ee2e7e7
Karl-Michaud <karl.michaud@mail.utoronto.ca>
2025-11-24 12:46:05 -0500

    Resume update nov 24

--- website/src/app/components/Navigation.tsx
+++ website/src/app/components/Navigation.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { useState, useEffect, useRef } from "react";
 import styles from './Navigation.module.css';
 
-const PDF_URL = '/Karl_Alexandre_Michaud_Resume_Nov_5.pdf';
+const PDF_URL = '/Karl_Alexandre_Michaud_Resume_Nov_24.pdf';
 
 export default function Navigation() {
   const [activeSection, setActiveSection] = useState('home');
